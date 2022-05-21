371bbe3dc1ce0f60cb64871da567dcb98dde1c65
STOshka <34401821+STOshka@users.noreply.github.com>
2022-05-21 11:38:28 +0300

    fix: eslint to 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-const path = require('node:path');
+const path = require('path');
 
 const stream = fs.createReadStream(
-    path.join(__dirname + '/text.txt'), 'utf8'
+  path.join(__dirname + '/text.txt'), 'utf8'
 );
 
 stream.on('readable', () => {
-    const data = stream.read();
-    if (data != null) console.log(data);
-})
+  const data = stream.read();
+  if (data != null) console.log(data);
+});
